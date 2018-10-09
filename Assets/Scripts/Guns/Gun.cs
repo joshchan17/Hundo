32d024347ad9405ef3f5219f1d0c6748d8da24b8
Chris Heuer <chrisdheuer@gmail.com>
2018-10-09 00:54:04 -0500

    Added fire rate/cooldown in the Gun script

--- Assets/Scripts/Guns/Gun.cs
+++ Assets/Scripts/Guns/Gun.cs
@@ -1,18 +1,32 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public abstract class Gun : Item {
 
 	public Transform SpawnPoint;
 	public Projectile ProjectilePrefab; // Has events for on collide
 	public ProjectileUpdate ProjectileUpdate;
+    public AudioSource FireSound;
+	public float FireRate;
+	private bool canFire;
 
-    public AudioSource gunShot;
+	public Gun() {
+		canFire = true;
+	}
 
 	protected abstract void Fire();
 
 	public override void Use() {
+		if (canFire) {
+			StartCoroutine(fireAndWait());
+		}
+	}
+
+	private IEnumerator fireAndWait() {
+		canFire = false;
 		Fire();
-		//FiringPattern.Fire(ProjectilePrefab, SpawnPoint, ProjectileUpdate);
-        gunShot.Play();
+		FireSound.Play();
+		yield return new WaitForSeconds(FireRate);
+		canFire = true;
 	}
 }
--- Assets/Scripts/Guns/Pistol.cs
+++ Assets/Scripts/Guns/Pistol.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 
 public class Pistol : Gun {
-
 	protected override void Fire() {
 		Debug.Log("Fire!");
 	}

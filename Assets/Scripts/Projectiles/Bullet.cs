3f96e6e2e5abacbb063d44b60c2ae9e0e0a41ce3
Chris Heuer <chrisdheuer@gmail.com>
2018-10-09 01:42:06 -0500

    Moved abstract methods out of projectile until its needed later

--- Assets/Scripts/Projectiles/Bullet.cs
+++ Assets/Scripts/Projectiles/Bullet.cs
@@ -5,6 +5,13 @@ public class Bullet : Projectile {
 	public float Damage;
 
 	private void OnCollisionEnter2D(Collision2D coll) {
+		Damageable damageable;
+		if (coll.gameObject.tag == "Enemy"
+			&& (damageable = coll.gameObject.GetComponent<Damageable>()) != null)
+		{
+			damageable.Damage(Damage);
+		}
+
 		Destroy(gameObject);
 	}
 }
